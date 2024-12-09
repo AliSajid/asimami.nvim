ee6fe81563bc272f7a13af32721d589b8663c965
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-12-09 09:55:14 -0500

    fix: update the config for neo-tree
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -2,8 +2,8 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
+  [1] = 'nvim-neo-tree/neo-tree.nvim',
+  version = 'v3.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
@@ -11,13 +11,20 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+    { [1] = '\\', [2] = ':Neotree float reveal<CR>', desc = 'NeoTree reveal' },
   },
   opts = {
+    close_if_last_window = true,
     filesystem = {
       window = {
+        position = 'float',
+        mapping_options = {
+          noremap = true,
+          nowait = true,
+        },
         mappings = {
           ['\\'] = 'close_window',
+          ['<TAB>'] = 'toggle_node',
         },
       },
     },
