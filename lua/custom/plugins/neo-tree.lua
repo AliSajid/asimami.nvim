b110a75cd9f1d09bcd9448d0e5bc2739a0ed8972
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-12-09 09:55:14 -0500

    fix: update the config for neo-tree
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,19 +1,31 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
-vim.pack.add {
-  { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
-  'https://github.com/nvim-lua/plenary.nvim',
-  'https://github.com/MunifTanjim/nui.nvim',
-}
-
-vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
-
-require('neo-tree').setup {
-  filesystem = {
-    window = {
-      mappings = {
-        ['\\'] = 'close_window',
+return {
+  [1] = 'nvim-neo-tree/neo-tree.nvim',
+  version = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { [1] = '\\', [2] = ':Neotree float reveal<CR>', desc = 'NeoTree reveal' },
+  },
+  opts = {
+    close_if_last_window = true,
+    filesystem = {
+      window = {
+        position = 'float',
+        mapping_options = {
+          noremap = true,
+          nowait = true,
+        },
+        mappings = {
+          ['\\'] = 'close_window',
+          ['<TAB>'] = 'toggle_node',
+        },
       },
     },
   },
