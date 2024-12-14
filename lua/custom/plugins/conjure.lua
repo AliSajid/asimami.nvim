c7c254a00eae565b5d936f55e31648a9b0cc090a
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-12-13 19:14:34 -0500

    feat: add conjure for lisps and schemes
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

---
+++ lua/custom/plugins/conjure.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    [1] = 'Olical/conjure',
+    ft = { 'clojure', 'fennel', 'python', 'lua', 'Racket', 'Scheme' }, -- etc
+    init = function()
+      vim.g['conjure#extract#tree_sitter#enabled'] = true
+      -- Set configuration options here
+      -- Uncomment this to get verbose logging to help diagnose internal Conjure issues
+      -- This is VERY helpful when reporting an issue with the project
+      -- vim.g["conjure#debug"] = true
+    end,
+
+    -- Optional cmp-conjure integration
+    dependencies = { 'PaterJason/cmp-conjure', 'Grazfather/sexp.nvim' },
+  },
+  {
+    [1] = 'PaterJason/cmp-conjure',
+    config = function()
+      local cmp = require 'cmp'
+      local config = cmp.get_config()
+      table.insert(config.sources, { name = 'conjure' })
+      return cmp.setup(config)
+    end,
+  },
+  {
+    [1] = 'Grazfather/sexp.nvim',
+    config = true,
+  },
+}
