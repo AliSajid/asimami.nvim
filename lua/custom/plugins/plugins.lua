b747c7c90445f830f867221e233f173b7ba09f7b
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-05-16 10:28:01 -0400

    fix: correct the option name
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- lua/custom/plugins/plugins.lua
+++ lua/custom/plugins/plugins.lua
@@ -1,10 +1,10 @@
 return {
-  'derektata/lorem.nvim',
+  [1] = 'derektata/lorem.nvim',
   config = function()
     require('lorem').opts {
-      sentenceLength = 'medium',
+      sentence_length = 'medium',
       comma_chance = 0.2,
-      max_commas_per_sentence = 2,
+      max_commas = 2
     }
   end,
 }
